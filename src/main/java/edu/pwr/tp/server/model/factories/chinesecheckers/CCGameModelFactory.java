70833b99cb5266d2a78a8941e6faf7278e11e017
NJ4r0 <236495@student.pwr.edu.pl>
2017-12-10 21:07:23 +0100

    repaired putting pawns to a board (most easy mode)

--- src/main/java/edu/pwr/tp/server/model/factories/chinesecheckers/CCGameModelFactory.java
+++ src/main/java/edu/pwr/tp/server/model/factories/chinesecheckers/CCGameModelFactory.java
@@ -8,6 +8,8 @@ import edu.pwr.tp.server.model.elements.chinesecheckers.CCPawn;
 import edu.pwr.tp.server.model.elements.chinesecheckers.CCPlayer;
 import edu.pwr.tp.server.model.factories.GameModelFactory;
 
+import java.util.ArrayList;
+
 public class CCGameModelFactory extends GameModelFactory {
 
     private CCGameModelFactory(){
@@ -36,10 +38,43 @@ public class CCGameModelFactory extends GameModelFactory {
         CCPlayer[] ccPlayers = new CCPlayer[players];
         for(int i = 0; i<players; i++){
             ccPlayers[i] = (CCPlayer) playerFactory.createPlayer();
-            CCPawn[] pawns = new CCPawn[CCBoard.pawnsPerPlayer];
-            for(int j=0; j<CCBoard.pawnsPerPlayer; j++){
-                pawns[j] = (CCPawn) pawnFactory.createPawn(ccPlayers[i].hashCode());
-                //TODO : place pawns on the board
+            switch (i){
+                case 0:
+                    for(int j=4; j<=7; j++)
+                        for(int k=4; k<=7; k++)
+                            if(j+k<=11)
+                                board.putPawn(j,k,new CCPawn(ccPlayers[i].hashCode()));
+                    break;
+                case 1:
+                    for(int j=13; j<=16; j++)
+                        for(int k=4; k<=7;k++)
+                            if(j+k<=20)
+                                board.putPawn(j,k,new CCPawn(ccPlayers[i].hashCode()));
+                    break;
+                case 2:
+                    for(int j=4; j<=7; j++)
+                        for(int k=13; k<=16;k++)
+                            if(j+k<=20)
+                                board.putPawn(j,k,new CCPawn(ccPlayers[i].hashCode()));
+                    break;
+                case 3:
+                    for(int j=0; j<=3; j++)
+                        for(int k=9; k<=12; k++)
+                            if(j+k>=12)
+                                board.putPawn(j,k,new CCPawn(ccPlayers[i].hashCode()));
+                    break;
+                case 4:
+                    for(int j=9; j<=12; j++)
+                        for(int k=9; k<=12; k++)
+                            if(j+k>=21)
+                                board.putPawn(j,k,new CCPawn(ccPlayers[i].hashCode()));
+                    break;
+                case 5:
+                    for(int j=9; j<=12; j++)
+                        for(int k=0; k<=3; k++)
+                            if(j+k>=12)
+                                board.putPawn(j,k,new CCPawn(ccPlayers[i].hashCode()));
+                    break;
             }
         }
         model.setBoard(board);
