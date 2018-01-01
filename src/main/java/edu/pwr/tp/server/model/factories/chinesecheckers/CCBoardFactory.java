6ba1360dddcd1540d225386b3941ad6cfa7b6e5d
NJ4r0 <236495@student.pwr.edu.pl>
2018-01-01 18:09:21 +0100

    fixed creation to add baseID flag

--- src/main/java/edu/pwr/tp/server/model/factories/chinesecheckers/CCBoardFactory.java
+++ src/main/java/edu/pwr/tp/server/model/factories/chinesecheckers/CCBoardFactory.java
@@ -20,8 +20,18 @@ public class CCBoardFactory extends BoardFactory{
         CCField[][] fields = new CCField[17][17];
         for(int x=0; x<17; x++)
             for(int y=0; y<17; y++){
-                if(x<=12&&y<=12&&x+y>=12) fields[x][y] = new CCField(x, y);
-                else if(x>=4&&y>=4&&x+y<=20) fields[x][y] = new CCField(x,y);
+                if(x<=12&&y<=12&&x+y>=12){
+                    if(x<=3&&y>=9) fields[x][y] = new CCField(x,y,1);
+                    else if (x>=9&&y<=3) fields [x][y] = new CCField(x,y,3);
+                    else if (x+y>=21&&x>=9&&y>=9) fields[x][y] = new CCField(x,y,5);
+                    else fields[x][y] = new CCField(x, y, -1);
+                }
+                else if(x>=4&&y>=4&&x+y<=20){
+                    if(x<=7&&y>=13) fields[x][y] = new CCField(x,y,0);
+                    else if (x+y<=11&&x<=7&&y<=7) fields[x][y] = new CCField(x,y,2);
+                    else if (x>=13&&y<=7) fields[x][y] = new CCField(x,y,4);
+                    else fields[x][y] = new CCField(x,y, -1);
+                }
                 else fields[x][y] = null;
             }
         return new CCBoard(fields);
